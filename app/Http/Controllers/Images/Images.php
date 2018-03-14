d9a7716bc38877ce299bb823bf571ad2e9495d1a
Christopher Quezada <cpqm07@gmail.com>
2018-03-14 11:55:49 -0300

    Images Table

---
+++ app/Http/Controllers/Images/Images.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Images;
+
+use App\Images;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class Images extends ApiController
+{
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Images $images)
+    {
+        //
+    }
+
+    public function edit(Images $images)
+    {
+        //
+    }
+
+    public function update(Request $request, Images $images)
+    {
+        //
+    }
+
+    public function destroy(Images $images)
+    {
+        //
+    }
+}
