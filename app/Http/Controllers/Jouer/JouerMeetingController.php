09c84130394527678205426ad625db5a0aed84d4
Christopher Quezada <cpqm07@gmail.com>
2018-04-18 18:26:01 -0300

    AddMeetings

--- app/Http/Controllers/Jouer/JouerMeetingController.php
+++ app/Http/Controllers/Jouer/JouerMeetingController.php
@@ -22,26 +22,16 @@ class JouerMeetingController extends ApiController
 
     public function addMeeting(Jouer $jouer, Meeting $meetings)
     {
-        $jouer_match_start = $jouer->meetings()->get()->pluck('time_begin');
-        $jouer_match_end = $jouer->meetings()->get()->pluck('time_end');
+        $jouer_matches = $jouer->meetings()->get();
 
-        var_dump($jouer_match_start); die();
-
-        /*
-        $current_match = $meetings->time_begin;
-
-        foreach ($jouer_matches_time as $time) {
-            if ($time !== $current_match) {
-              echo "El partido $time No es a la misma hora que el actual: $current_match " . "\n" ;
-            }
-        }*/
-
-
-        //var_dump($current_match); die();
-        //var_dump($matches);
-        //$date = $meetings->time_begin;
-        //$jouer->meetings()->attach(array($meetings->id));
-        //return $this->showAll($matches);
+        foreach ($jouer_matches as $match) {
+          if ($meetings->time_begin >= $match->time_begin && $meetings->time_end <= $match->time_end) {
+            return $this->errorResponse('No es posible agendar el encuentro, procura que el horario no coincida con otro encuentro.', 400);
+          } else {
+            $jouer->meetings()->attach(array($meetings->id));
+            return $this->showAll($jouer->meetings()->get());
+          }
+        }
     }
 
     public function removeMeeting(Jouer $jouer, Request $request)
