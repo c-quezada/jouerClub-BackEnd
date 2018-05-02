e5422fa37f67b30e9eec570414cbcfca8c791a02
Christopher Quezada <cpqm07@gmail.com>
2018-05-02 12:27:33 -0300

    Corrections

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -14,12 +14,12 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'            => 'alpha|required|max:25|min:4',
-            'lastname'        => 'alpha|required|max:25|min:6',
+            //'name'            => 'alpha|required|max:25|min:4',
+            //'lastname'        => 'alpha|required|max:25|min:6',
             'nickname'        => 'required|max:30|min:unique',
             'email'           => 'required|email|unique:users',
             'password'        => 'required|min:6',
-            'avatar'          => 'required|image',
+            //'avatar'          => 'required|image',
             'type'            => 'required|min:4',
             'phone'           => 'required|min:9|max:11',
         ];
--- database/migrations/2014_10_12_000000_users_table.php
+++ database/migrations/2014_10_12_000000_users_table.php
@@ -15,13 +15,13 @@ class UsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->comment("field to store user's number idenfifier");
-            $table->string('name', 100)->comment("field to store user's name, limit 25");
-            $table->string('lastname', 25)->comment("field to store user's lastname, limit 25");
+            $table->string('name', 100)->comment("field to store user's name, limit 25")->nullable();
+            $table->string('lastname', 25)->comment("field to store user's lastname, limit 25")->nullable();
             $table->string('nickname', 30)->comment("field to store coach's nickname")->unique();
             $table->string('email', 50)->comment("field to store user's email that must be unique, limimt 30")->unique();
             $table->string('phone', 12)->comment("field to store user's phone with his area code, must be unique, limimt 12")->unique();
             $table->string('password')->comment("field to store user's encrypted password, whit bcrypt helper function - LARAVEL");
-            $table->string('avatar')->comment("field to store user's picture profile")->default('profile');
+            $table->string('avatar')->comment("field to store user's picture profile")->default('profile')->nullable();
             $table->enum('status', ['verified', 'pending'])->comment("field to store user's status")->default('pending');
             $table->enum('type', ['admin', 'dev', 'jouer', 'cluber', 'coach'])->comment("field to store user's type");
             $table->string('code_verification', 40)->comment("field to store user's verification code, this define the user's status");
