6b0594e7686db88d3220915a375aacf5058b03f1
Christopher Quezada <cpqm07@gmail.com>
2018-06-09 19:45:40 -0400

    Carbon updated

--- app/Http/Controllers/Court/CourtMeetingsController.php
+++ app/Http/Controllers/Court/CourtMeetingsController.php
@@ -11,18 +11,19 @@ class CourtMeetingsController extends ApiController
 {
 		public function __construct()
 		{
-			//parent::__construct();
+			parent::__construct();
 		}
 		
     public function store(Court $court, Request $request)
     {    
+        $today_message = Carbon::today()->format('l jS \\of F Y');
         $all_meetings = $court->meetings()->get();
 
         if (!isset($request->date)) {
             
             $current_date_begin = Carbon::today()->addHours(7);
-            $current_date_end   = Carbon::today()->addHours(23);
-
+            $current_date_end   = Carbon::today()->addHours(23)->addMinutes(59);
+            
             if (count($all_meetings) > 0) {
                 $collection = collect();
 
@@ -34,16 +35,17 @@ class CourtMeetingsController extends ApiController
                 }
                 return $this->showAll($collection);
                 
-            } 
+            } else {
+                return $this->errorResponse("Hasta el momento, no se han registrado encuentros deportivos para la fecha: {$today_message}", 404);
+            }
 
         } else {
 
-            $custom_date   = Carbon::parse($request->date);
+            $day_message  = Carbon::parse($request->date)->format('l jS \\of F Y');
+
+            $date_begin   = Carbon::parse($request->date)->addHours(7);
             
-            echo
-            $date_begin    = $custom_date->addHours(7) . " " .
-            $date_end      = $custom_date->addHours(23)
-            ;
+            $date_end     = Carbon::parse($request->date)->addHours(23)->addMinutes(59);
 
             if (count($all_meetings) > 0) {
                 $collection = collect();
@@ -57,6 +59,8 @@ class CourtMeetingsController extends ApiController
 
                 return $this->showAll($collection);
                 
+            } else {
+                return $this->errorResponse("Hasta el momento, no se han registrado encuentros deportivos para la fecha: {$day_message}", 404);
             }
 
         }
